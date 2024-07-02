1744c9dbfd4b76bc7a4e733b9ae45311f2325fbb
Morgan <lef.morgan@gmail.com>
2024-07-02 15:33:10 +0200

    call login child component

--- discover-ngrx-workspavce/projects/authent-appli/src/app/app.routes.ts
+++ discover-ngrx-workspavce/projects/authent-appli/src/app/app.routes.ts
@@ -18,7 +18,7 @@ export const routes: Routes = [
     },
     {
         path: '**',
-        redirectTo: 'authenticate',
+        redirectTo: 'authenticate/login',
         pathMatch: 'full'
     }
 ];
